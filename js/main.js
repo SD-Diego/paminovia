f02b2845e901a511d8afb297fcc43a08678a8e4a
SD-Diego <79714481+SD-Diego@users.noreply.github.com>
2023-12-16 16:27:29 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const windowBackground = document.getElementById("window-background");
+const windowContainer = document.getElementById("window-container");
+const openButton = document.getElementById("open-button");
+const closeButton = document.getElementById("close-button");
+
+windowBackground.style.display = 'flex';
+
+openButton.addEventListener('click', () => {
+    windowBackground.style.display = 'flex';
+}); 
+
+const closeWindow = () => {
+    windowContainer.classList.add("close")
+    setTimeout(() => {
+        windowContainer.classList.remove("close")
+        windowBackground.style.display = 'none'
+    }, 500);
+}
+
+closeButton.addEventListener('click', () => closeWindow() ); 
+
+window.addEventListener('click', e => e.target == windowBackground && closeWindow())
+
+/*
+document.querySelectorAll('.container-img img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.lightbox-background').style.display='block';
+        document.querySelector('.lightbox-background img').src = image.getAttribute('src');
+    }
+});
+*/
+
+function capturarSrc(event) {
+    var contenedor = event.currentTarget;
+    var imagen = contenedor.querySelector('img');
+    var srcImagen = imagen.getAttribute('src');
+    document.querySelector('.lightbox-background').style.display='grid';
+    document.querySelector('.lightbox-background img').src = srcImagen;
+}
+document.querySelector('.lightbox-background').onclick = () =>{
+    document.querySelector('.lightbox-background').style.display='none';
+}
+
+document.querySelector('.lightbox-background span').onclick = () =>{
+    document.querySelector('.lightbox-background').style.display='none';
+}
